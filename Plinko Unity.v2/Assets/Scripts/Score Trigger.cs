53ad7273389a789602c06aa4c050ebf0c50ded7e
SebastianP2423 <000946282@mohawkcollege.ca>
2025-12-11 01:46:23 -0500

    Modified the score trigger script into a score multiplier that increments its multiplier with each bucket (highest to lowest)

--- Plinko Unity.v2/Assets/Scripts/Score Trigger.cs
+++ Plinko Unity.v2/Assets/Scripts/Score Trigger.cs
@@ -4,10 +4,13 @@ public class ScoreTrigger : MonoBehaviour
 {
     public ScoreKeeper scoreKeeper;
     public int points = 1;
+    public int multiplier = 1;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        scoreKeeper.AddScore(points);
+        if (!collider.CompareTag("Disc")) return;
+        int finalScore = points * multiplier;
+        scoreKeeper.AddScore(finalScore);
         Destroy(collider.gameObject);
     }
 }
